bc062fd692058cd4359f67bf1362aaeee36e7923
Sazard <nicolas.sutter@epita.fr>
2020-03-11 16:15:29 +0000

    exo

---
+++ Lab5/js/greeting.js
@@ -0,0 +1,86 @@
+
+
+
+//Questions
+
+var firstName = 'Nicolas';
+var favouriteColor = 'red';
+
+var favouriteFood = 'tomatoe';
+var city = 'Paris';
+
+
+//display firstname fav color, fav food ans city
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood +' '+ city);
+
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+
+var endingPhrase= ' is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase +' ' + one +', '+ two +', '+ three +', '+ four +', '+five +', '+ six +', '+seven + endingPhrase);
+
+//codecademy
+//step 2
+
+console.log(99);
+console.log(0);
+
+//step 3
+
+//Opening line
+console.log('It was love at first sight.');
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+//step 4
+
+console.log("JavaScript");
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+//step 5
+
+console.log(18.0+3.5);
+console.log(2020-1969);
+console.log(65.0/240.0);
+console.log(0.2708*100);
+
+//step 6
+
+console.log('Hello'+'World');
+console.log('Hello '+'World');
+
+//step 7
+
+console.log('Teaching the world how to code'.length);
+
+//step 8
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//step 9
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
